f17561cfd521446a128a83bcbe8b265e509730f9
yd94 <yd94@duke.edu>
2018-03-31 15:37:31 -0400

    Add the SQL actions after Professor first and second click in Professor_Click.java

--- Server/src/main/java/LTI_message.java
+++ Server/src/main/java/LTI_message.java
@@ -1,4 +1,3 @@
-
 public class LTI_message{
 	//this is the class that save the message from LTI, which contains information about the people and the course and the way to communicate with LTI
 	String course_id;	//unique id for account and each course
