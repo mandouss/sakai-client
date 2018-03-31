674574fc2d8a72891404a70541766c2867848de8
yd94 <yd94@duke.edu>
2018-03-31 14:14:23 -0400

    add box dependencies, and add course_name into LTI_message class

--- Server/src/main/java/LTI_message.java
+++ Server/src/main/java/LTI_message.java
@@ -1,16 +1,18 @@
 
 public class LTI_message{
 	//this is the class that save the message from LTI, which contains information about the people and the course and the way to communicate with LTI
-	String context_id;	//unique id for account and each course
+	String course_id;	//unique id for account and each course
 	String return_url;	//send message to this url(Sakai)
 	String roles;		//Instructor, Student
 	String person_email;//email of this account
+	String Course_Name;
 
-	public LTI_message(String context_id,String return_url,String roles,String person_email){
+	public LTI_message(String context_id,String return_url,String roles,String person_email,String course_name){
 		//constructor
-		this.context_id=context_id;
+		this.course_id=context_id;
 		this.return_url=return_url;
 		this.roles=roles;
 		this.person_email=person_email;
+		this.Course_Name=course_name;
 	}
 }
