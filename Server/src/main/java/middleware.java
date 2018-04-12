e2c83be43a2362e4bbbb5c4284607a1ce6390728
yd94 <yd94@duke.edu>
2018-04-11 22:13:24 -0400

    Merged different people's code into the main function in middleware.java. Delete the database part code because we don't need database on our server. Added box_folder_info class to store folder_info we want. Moved some outdated code to OutDated folder.

--- Server/src/main/java/middleware.java
+++ Server/src/main/java/middleware.java
@@ -1,39 +1,41 @@
 import java.util.*;
 
 public class middleware {
-    HashMap<String, String> course_id_folder_id_map;
+    //HashMap<String, String> course_id_folder_id_map;
 
     boolean connect() { //Fangyuan Sun
         return true;
     }
 
-    boolean check_course_id_exist(String course_id) {  //Fangyuan Sun
+    //Please realize the three functions in Box_helper class
+    /*boolean Check_Course_id(String course_id) {  //Fangyuan Sun  return true;  }
 
-        return true;
-    }
+    String Get_Folder_id(String course_id){return "";}
 
-    String box_create_folder(String course_name) {  //Sihao Yao
-        return "";
-    }
+    String Create_New_Folder(String course_id, String course_name) {  //Sihao Yao        return "";    }
 
-    String box_get_folder_info(String folder_id) {  //Luoyi Li
-        return "";
-    }
+    String Get_Folder_Info(String folder_id) {  //Luoyi Li        return "";    }*/
 
     public static void main(String[] args) {
         middleware my_middleware = new middleware();
         LTI_message test_msg = new LTI_message("ECE651_ID_12345", "127.0.0.1", "professor", "wz88@duke.edu", "ECE_651");
 
+        Box_Helper helper = new Box_Helper();
         String folder_id;
         if (test_msg.roles.equals("professor")) {
-            if (!my_middleware.check_course_id_exist(test_msg.course_id)) {
+            Professor professor=new Professor();
+            Professor.Click(test_msg,helper);
+            /*if (!my_middleware.check_course_id_exist(test_msg.course_id)) {
                 folder_id = my_middleware.box_create_folder(test_msg.Course_Name);
                 my_middleware.course_id_folder_id_map.put(test_msg.course_id, folder_id);
             } else {
                 // course_id exists, then folder_id must exist
                 folder_id = my_middleware.course_id_folder_id_map.get(test_msg.course_id);
             }
-            my_middleware.box_get_folder_info(folder_id);
+            my_middleware.box_get_folder_info(folder_id);*/
+        }else (test_msg.roles.equals("student")){
+            Student student=new Student();
+            student.Click(test_msg,helper);
         }
     }
 }
